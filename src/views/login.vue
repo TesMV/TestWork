83259e0163633fb7745fce23713867d4cc876c63
TesMV <teselkin29@gmail.com>
2020-07-18 17:19:08 +0400

    Init commit

---
+++ src/views/login.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="login">
+        <input type="text" name="username" v-model="input.username" placeholder="Username" />
+        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+        <button type="button" v-on:click="login()">Login</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+body {
+    width: 250px;
+    background-color: #839d8f;
+    margin: 0px auto;
+}
+#login {
+    border: 3px solid #63e39a;
+    border-radius: 5px;
+    padding: 20px;
+    margin-top: 50px;
+	text-align:center;
+}
+input{
+	margin:10px;
+}
+</style>
---
+++ src/views/secure.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="secure">
+        <img src="../static/img/user.png" alt="user">
+        <h1>Имя Фамилия</h1>
+        <p id="email"></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Secure',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style>
+#secure {
+         width: 900px;
+		margin: 0px auto;
+		background-color: #839d8f;
+    }
+img {
+    float: left;
+    margin-right: 30px;
+    width: 300px;
+    height: 300px;
+}
+</style>
